d3b3afb4b2fabd7f985ff341816975058a52b014
kiran-kumar011 <kirankirankumar56@gmail.com>
2019-02-13 22:05:04 +0530

    focus2

--- assets/stylesheet/main.js
+++ assets/stylesheet/main.js
@@ -2,52 +2,77 @@ var excelSheet = document.querySelector(".main-sheet");
 var grid = document.createElement("section");
 grid.classList.add("sheet");
 
-var excelData = localStorage.getItem("data-key") || [];
+var excelData = localStorage.getItem("data") || [];
 
-// for (var j = 0; j < 27; i++){
-	
 
-	for(var i = 0; i < 2700; i++){
-		var sheetInput = document.createElement("input");
-		sheetInput.classList.add("text-input");
-		sheetInput.setAttribute("data-key", i);
-		sheetInput.focus();
-		grid.appendChild(sheetInput);
-	}
-// }
+for(var i = 0; i < 2700; i++){
+	var sheetInput = document.createElement("input");
+	sheetInput.addEventListener('blur', handleBlur );
+	sheetInput.classList.add("text-input");
+	sheetInput.setAttribute("data-key", i);
+	sheetInput.value = localStorage.getItem(`I${i}`) || ''; 
+	grid.appendChild(sheetInput);
+}
 excelSheet.appendChild(grid);
 
 // ===================================================================
 // sheet data function which is pushing an string into the main array
 // ===================================================================
-function sheetData(){
-	var inputValue = sheetInput.value;
-	excelData.push(inputValue);
-	localStorage.setItem("data-key", inputValue);
-	display(excelData);
-}
 
 // ===================================================================
 // enter click function
 // ===================================================================
 
-function enterClick(e){
-	// console.log(e);
-	e.preventDefault();
-	if(e.keyCode == 13){
-		sheetData();
+
+excelSheet.appendChild(grid);
+
+
+// name editing and storing
+var fileName = document.querySelector('.fileName');
+
+function editFileName(e){
+	// console.dir(e.target);
+	var edit = e.target.dataset.id;
+	var parent = e.target.parentElement;
+	var input = document.createElement('input');
+	input.type = 'text';
+	input.classList.add('editFileName')
+	input.value = e.target.textContent;
+	parent.replaceChild(input, e.target);
+	input.focus();
+	input.addEventListener('blur', (e)=> {
+		let val = e.target.value;
+	let parent = e.target.parentElement;
+	let span = document.createElement('span');
+	span.classList.add('fileName');
+	span.textContent =val;
+		parent.replaceChild(span, e.target);
+
+
+	})
+}
+
+// focusing the element which is clicked.
+var inputContainer = document.querySelector('.sheet');
+// var inputElm = document.querySelectorAll('.text-input');
+console.dir(inputContainer);
+function handleBorder(e) {
+	console.log(e.target);
+	var key = e.target.dataset.key;
+	if(e.target.dataset.key){
+			e.target.classList.add('displayBorder');
+			// e.target.focus();
 	}
 }
 
-function display(){
-	sheetInput.innerText = excelData;
-	localStorage.getItem("data-key");
+function handleBlur(e) {
+	let value = e.target.value;
+	e.target.classList.remove('displayBorder');
+	let key = e.target.dataset.key;
+	localStorage.setItem(`I${key}`,value);
 }
 
-display(sheetData);
-document.addEventListener("blur", (e) => {
-	console.log(e)
-	sheetData();
-})
+inputContainer.addEventListener('click', handleBorder);
+// file name editing and storing.
+fileName.addEventListener('dblclick', editFileName);
 
-document.addEventListener("keyup", enterClick);
