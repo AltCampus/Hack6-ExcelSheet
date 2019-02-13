00698f37d71d31fb7307e23567dc838eebed7ee8
kiran-kumar011 <kirankirankumar56@gmail.com>
2019-02-13 16:50:51 +0530

    creating grid

---
+++ assets/stylesheet/main.js
@@ -0,0 +1,17 @@
+// grid creation 
+// var gridParent = document.querySelector('.container');
+// var ul = document.createElement('ul');
+
+// var col = '';
+
+// ul.innerHTML = col;
+// gridParent.innerHTML = ul;
+// function grid() {
+// for (i=0; i < 27; i++){
+// 	col += `<li calss="list" data-id=${i}>i</li>`;
+// 	for (j=0; j < 100; j++){
+// 		col += `li calss="list" data-id=${i+j}>j</li>`
+// 	}
+// }
+// }
+// gridParent.innerHTML = col;
