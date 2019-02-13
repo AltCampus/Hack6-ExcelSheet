6545aa07744b17d0151ddb845d724a429b55f472
sajan <sajanloveu123@gmail.com>
2019-02-13 18:51:30 +0530

    localStorage implimentation

--- assets/stylesheet/main.js
+++ assets/stylesheet/main.js
@@ -2,12 +2,44 @@ var excelSheet = document.querySelector(".main-sheet");
 var grid = document.createElement("section");
 grid.classList.add("sheet");
 
+var excelData = localStorage.getItem("data") || [];
+
+
 for(var i = 0; i < 2700; i++){
 	var sheetInput = document.createElement("input");
 	sheetInput.classList.add("text-input");
-	// sheetInput.setAttibute();
+	sheetInput.setAttribute("data-key", i);
 	grid.appendChild(sheetInput);
 }
-
-
 excelSheet.appendChild(grid);
+
+// ===================================================================
+// sheet data function which is pushing an string into the main array
+// ===================================================================
+function sheetData(){
+	var inputValue = sheetInput.value;
+	excelData.push(inputValue);
+	localStorage.setItem("data",inputValue);
+	display(excelData);
+	console.log(inputValue);
+}
+
+// ===================================================================
+// enter click function
+// ===================================================================
+function enterClick(e){
+	console.log(e);
+	e.preventDefault();
+	if(e.keyCode == 13){
+		sheetData();
+	}
+}
+
+function display(){
+	// sheetInput.innerText = excelData;
+	localStorage.getItem("data");
+}
+display(sheetData);
+
+document.addEventListener("keyup", enterClick);
+
